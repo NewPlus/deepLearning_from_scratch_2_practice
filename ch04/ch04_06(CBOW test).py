05bda899f7d26af8738ddacf7fcce08e29a1f156
NewPlus <32642002+NewPlus@users.noreply.github.com>
2022-05-09 20:50:43 +0900

    ch 04. word2vec 개선 예제(Embedding, NegativeSqmpling)

---
+++ ch04/ch04_06(CBOW test).py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('..')
+from common.util import most_similar, analogy
+import pickle
+
+pkl_file = r'C:\Users\lyhth\OneDrive\Desktop\Programming\git\deepLearning_from_scratch_2_practice\deepLearning_from_scratch_2_practice\ch04\cbow_params.pkl'
+
+with open(pkl_file, 'rb') as f:
+    params = pickle.load(f)
+    word_vecs = params['word_vecs']
+    word_to_id = params['word_to_id']
+    id_to_word = params['id_to_word']
+
+querys = ['you','year','car','toyota']
+for query in querys:
+    most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
+
+analogy('king','man','queen',word_to_id, id_to_word, word_vecs)
+analogy('take','took','go',word_to_id, id_to_word, word_vecs)
+analogy('car','cars','child',word_to_id, id_to_word, word_vecs)
+analogy('good','better','bad',word_to_id, id_to_word, word_vecs)
